a6f7a7f34fb0f42f3b0cc86cd2f075abda8d14dd
Mohit kumar <mohitkumar120904@gmail.com>
2024-11-12 13:08:06 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,16 +1,12 @@
-# Initialize an empty list
 lst = []
-
-# Read the number of commands
 n = int(input())
 
-# Loop through each command
 for _ in range(n):
-    # Read the command and split it into parts
+    #Read command and split it into parts
     command = input().split()
     cmd = command[0]
     
-    # Execute the appropriate command based on input
+    #Execute appropriate command based on input
     if cmd == "insert":
         i = int(command[1])
         e = int(command[2])
