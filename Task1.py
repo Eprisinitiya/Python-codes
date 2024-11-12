4970debd544b20dc1ecab46de64847833df80ff2
Mohit kumar <mohitkumar120904@gmail.com>
2024-11-12 12:46:46 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+#Print sum of the two numbers
+print(a + b)
+
+#Print difference of the two numbers
+print(a - b)
+
+#Print product of the two numbers
+print(a * b)
