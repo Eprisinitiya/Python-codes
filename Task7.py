07eea7bee009be9e3bb15c01eda6253a16308466
Mohit kumar <mohitkumar120904@gmail.com>
2024-11-18 22:57:32 +0530

    Create Task7.py

---
+++ Task7.py
@@ -0,0 +1,7 @@
+def name_string(name):
+    result = name + "Happy"
+    return result
+
+print(name_string("Mubashir"))  # -> "MubashirHappy"
+print(name_string("Matt"))      # -> "MattHappy"
+print(name_string("python"))    # -> "pythonHappy"
